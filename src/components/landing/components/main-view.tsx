bc45417b1c147c92dcc41018a251e73d3738c3a1
amirhoseinMardani <amir@hosein.mardani>
2025-08-16 15:55:07 +0330

    main-view top added!

--- src/components/landing/components/main-view.tsx
+++ src/components/landing/components/main-view.tsx
@@ -1,7 +1,28 @@
 const MainView: React.FC = () => {
     return (
-         <span className="view main">
-            <span>رزومه</span>
+        <span className="view main">
+            <span className="top">
+                <span className="actions">
+                    <button>action1</button>
+                    <button>action2</button>
+                </span>
+                <span className="title-logo">
+                    <span className="logo"></span>
+                    <span className="title">
+                        <span className="pre-title">معاونت نیروی انسانی</span>
+                        <span className="main-title">
+                            <span className="last">ارتش</span>
+                            <span> جمهوری اسلامی </span>
+                            <span className="first">ایران</span>
+                        </span>
+                    </span>
+                </span>
+            </span>
+            <span className="bottom">
+                <span className="item item-left">item-left</span>
+                <span className="item item-middle">item-middle</span>
+                <span className="item item-right">item-right</span>
+            </span>
         </span>
     )
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './fonts.scss';
+import './assets/button.scss';
+import './assets/fonts.scss';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
