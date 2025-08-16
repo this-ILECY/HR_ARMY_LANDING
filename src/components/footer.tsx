a62ba350e848215acd9f55897f6defe5f2f7a038
amirhoseinMardani <amir@hosein.mardani>
2025-08-16 17:53:10 +0330

    footer started!

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,20 @@
+import React from 'react';
+// import jalaali from 'jalaali-js';
+
 const Footer: React.FC = () => {
+
+//     const today = new Date();
+//   const { jy } = jalaali.toJalaali(today);
+
     return (
-        <>
-            <span>hello</span>
-        </>
+        <span className="footer">
+            <span className="links">
+
+            </span>
+            <span className="credit">
+                تولید شده توسط معاونت نیروی انسانی ستاد ارتش جمهوری اسلامی ایرانتولید شده توسط معاونت نیروی انسانی ستاد ارتش جمهوری اسلامی ایران 1404
+            </span>
+        </span>
     )
 
 }
